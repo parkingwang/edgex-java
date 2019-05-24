43b39820c0e0c54edafb73ed6e05179259d74057
yoojia.chen <yoojiachen@gmail.com>
2019-05-24 10:36:14 +0800

    Update and add topics

--- src/main/java/net/nextabc/edgex/ContextImpl.java
+++ src/main/java/net/nextabc/edgex/ContextImpl.java
@@ -46,8 +46,8 @@ final class ContextImpl implements Context {
     public Driver newDriver(Driver.Options opts) {
         checkCtx();
         this.serviceName = "Driver";
-        checkRequired(opts.name, "Trigger.Name MUST be specified");
-        checkRequired(opts.topics, "Trigger.Topic MUST be specified");
+        checkRequired(opts.name, "Driver.Name MUST be specified");
+        checkRequired(opts.topics, "Driver.Topic MUST be specified");
         return new DriverImpl(this.scoped, opts);
     }
 
