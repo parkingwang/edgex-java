6605a7ce91babcd2675ccfbde5a46dfe85c536de
yoojia.chen <yoojiachen@gmail.com>
2019-07-26 12:08:09 +0800

    Fixes

--- src/main/java/net/nextabc/edgex/Mqtt.java
+++ src/main/java/net/nextabc/edgex/Mqtt.java
@@ -6,17 +6,19 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
  * @author 陈哈哈 (yoojiachen@gmail.com)
  */
 class Mqtt {
-    
+
     static void setup(Globals globals, MqttConnectOptions opts) {
-        opts.setKeepAliveInterval(globals.mqttKeepAlive);
-        opts.setAutomaticReconnect(globals.mqttAutoReconnect);
-        opts.setMaxReconnectDelay(globals.mqttReconnectInterval * 1000);
-        opts.setConnectionTimeout(globals.mqttConnectTimeout);
-        opts.setCleanSession(globals.mqttClearSession);
-        if (null != globals.mqttUsername && !globals.mqttUsername.isEmpty()
-                && null != globals.mqttPassword && !globals.mqttPassword.isEmpty()) {
-            opts.setUserName(globals.mqttUsername);
-            opts.setPassword(globals.mqttPassword.toCharArray());
+        opts.setKeepAliveInterval(globals.getMqttKeepAlive());
+        opts.setAutomaticReconnect(globals.isMqttAutoReconnect());
+        opts.setMaxReconnectDelay(globals.getMqttReconnectInterval() * 1000);
+        opts.setConnectionTimeout(globals.getMqttConnectTimeout());
+        opts.setCleanSession(globals.isMqttClearSession());
+        final String username = globals.getMqttUsername();
+        final String password = globals.getMqttPassword();
+        if (null != username && !username.isEmpty()
+                && null != password && !password.isEmpty()) {
+            opts.setUserName(username);
+            opts.setPassword(password.toCharArray());
         }
     }
 }
