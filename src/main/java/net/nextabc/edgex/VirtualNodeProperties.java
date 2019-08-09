c363cf79010e4d6447c01f9e4482427e5c62aaea
yoojia.chen <yoojiachen@gmail.com>
2019-08-09 14:03:29 +0800

    Update and fixes

--- src/main/java/net/nextabc/edgex/VirtualNodeProperties.java
+++ src/main/java/net/nextabc/edgex/VirtualNodeProperties.java
@@ -11,7 +11,7 @@ import java.util.Map;
  */
 @Data
 @Builder
-class VirtualNodeProperties {
+public class VirtualNodeProperties {
 
     private final String uuid;
     private final String virtualId;
